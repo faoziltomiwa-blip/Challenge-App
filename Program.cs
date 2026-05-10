3fd964902b5af6dd418441912767e002506dca1e
Faozil <faoziltomiwa@gmail.com>
2026-05-10 14:31:11 +0100

    chore: update root endpoint message to test automated pipeline

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 // Basic Root endpoint to display a message confirming the API is running, along with environment info and timestamp
 app.MapGet("/", () => new {
-    Message = "API is running!",
+    Message = "API is running via automated pipeline",
     Environment = builder.Environment.EnvironmentName,
     Timestamp = DateTime.UtcNow
 });
